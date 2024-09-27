6ac53b592143dd7d740903311761560c128bb588
Samuelpeter001 <Samuelpeter61@gmail.com>
2024-09-27 04:59:18 +0100

    Update Learning02
    Job updates

---
+++ prepare/Learning02/Job.cs
@@ -0,0 +1,23 @@
+using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+    public class Job();
+    {
+        public string _company:
+        public string _jobTitle:
+        public int _startYear:
+        public int _endYear:
+        
+        public Job()
+        {
+        }
+
+        public void Display()
+        {
+            Console.WriteLine($"{_jobtitle} ({_company}) {_startYear}-{_endYear}. ");
+        }
+    }
+}
