24c1ce64dd7b68cdcee5e36084d6d8cd3e2f2b99
Samuelpeter001 <Samuelpeter61@gmail.com>
2024-09-27 16:45:37 +0100

    Update Resume.cs
    Finished Resume

--- prepare/Learning02/Resume.cs
+++ prepare/Learning02/Resume.cs
@@ -1,1 +1,19 @@
-f
+    using system
+
+    public class Resume();
+    {
+        public string _name;
+        public List<Job> jobs = new list<job>();
+    
+        public void Display()
+        {
+            Console.WriteLine($"Name: {_name}");
+            Console.WriteLine("Jobs:");
+
+            foreach (Job job in _jobs)
+            {
+                job.Display();
+            }
+        }
+    }
+}
