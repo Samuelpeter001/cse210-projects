13ee6630d69eabe1c5c87153c8d7e70b9886e7c4
Samuelpeter001 <samuelpeter61@gmail.com>
2024-10-06 00:31:22 +0100

    add file

--- prepare/Learning03/Program.cs
+++ prepare/Learning03/Program.cs
@@ -1,11 +1,27 @@
 using System;
 using System.Runtime.CompilerServices;
+using System.Xml.Schema;
+
+
 
 class program
+{\
+    static void Main(string[] args)
     {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("Hello Learning03 World!");
-        }
+        Fraction frac1 = new Fraction();
+        Control.WriteLine(frac1.GetFractionString());
+        Control.WriteLine(frac1.GetDecimalValue());
+
+        Fraction frac2 = new Fraction(5);
+        Control.WriteLine(frac2.GetFractionString());
+        Control.WriteLine(frac2.GetDecimalValue());
+
+        Fraction frac3 = new Fraction(3,4);
+        Control.WriteLine(frac3.GetFractionString());
+        Control.WriteLine(frac3.GetDecimalValue());
+
+        raction frac3 = new Fraction(1,3);
+        Control.WriteLine(frac4.GetFractionString());
+        Control.WriteLine(frac4.GetDecimalValue());
     }
 }
