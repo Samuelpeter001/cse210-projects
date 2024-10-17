03c1c87d7070b8dec6a3b48d49031205c3365f25
Samuelpeter001 <Samuelpeter61@gmail.com>
2024-10-17 14:58:57 +0100

    Update Program.cs

--- foundation/Foundation2/Program.cs
+++ foundation/Foundation2/Program.cs
@@ -1,9 +1,33 @@
 using System;
-
-class Program
+public class Program
 {
-    static void Main(string[] args)
+    public static void Main()
     {
-        Console.WriteLine("Hello Foundation2 World!");
+        // Create address
+        var address = new Address
+        {
+            Street = "18 Brave Street",
+            City = "Villa",
+            State = "Los Angeles",
+            Country = "USA"
+        };
+        var customer = new Customer
+        {
+            Name = "John Doe",
+            CustomerAddress = address
+        };
+        var product1 = new Product { Name = "KenLouis", ProductId = 1952, Price = 30.00, Quantity = 5 };
+        var product2 = new Product { Name = "Wristwatch", ProductId = 2115, Price = 10.00, Quantity = 2 };
+
+        var order = new Order
+        {
+            Order_Customer = customer
+        };
+        order.Products.Add(product1);
+        order.Products.Add(product2);
+
+        Console.WriteLine(order.PackingLabel());
+        Console.WriteLine(order.ShippingLabel());
+        Console.WriteLine($"Total Cost: ${order.TotalCost()}");
     }
 }
