6824012d7e7cb0c5e0548e0295351131717c16ad
Samuelpeter001 <Samuelpeter61@gmail.com>
2024-10-24 11:15:22 +0100

    Update PromptGenerator.cs

--- prove/Develop02/PromptGenerator.cs
+++ prove/Develop02/PromptGenerator.cs
@@ -1,13 +1,7 @@
-prompts = new List<string>
-     {
-         "What special thing happened today?"
-         "what did i learn today?"
-         "What progress did i make today?"
-         "How did i see the hand of the lord in my life today?"
-         "What was my profit tiday?"
-     }
-     public GetRandomPrompt()
-     {
-         Random random = new Random();
-         string prompt = prompts[random.Next(prompts.Count)];
-     }
+public class PromptGenerator
+{
+    public string GeneratePrompt()
+    {
+        return "What did you learn today?";
+    }
+}
