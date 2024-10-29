3b9a08ce85b1a7df70c61024e6a8a5b11350fed3
Samuelpeter001 <samuelpeter61@gmail.com>
2024-10-29 01:48:47 +0100

    Updated assignment

--- prove/Develop05/Program.cs
+++ prove/Develop05/Program.cs
@@ -11,16 +11,16 @@ class Program
     Console.Write("Choose an option: ");
     int option = Convert.ToInt32(Console.ReadLine());
 
-        switch (option)
-            {
-                case 1:
-                    breathingActivity.Run()
-                    break;
-                case 2:
-                    listingActivity.Run();
-                    break;
-                case 3:
-                    reflectingActivity.Run();
-                    break;
-            }
+    switch (option)
+    {
+        case 1:
+            breathingActivity.Run()
+            break;
+        case 2:
+            listingActivity.Run();
+            break;
+        case 3:
+            reflectingActivity.Run();
+            break;
+    }
 }
